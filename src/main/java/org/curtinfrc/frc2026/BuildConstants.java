f39ea530d901f3b1c85cf246a1111e1734ebb4fa
Sean Chan <155516197+prawny-boy@users.noreply.github.com>
2026-06-07 15:43:24 +0800

    fix: buildconstants folder issue (#3)

--- src/main/java/org/curtinfrc/frc2026/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2026/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "c7f1ba28fad2ca8cc9b3490a410cd142b3e3dc45";
-  public static final String GIT_DATE = "2026-05-10 03:38:52 GMT-04:00";
-  public static final String GIT_BRANCH = "sean/drive";
-  public static final String BUILD_DATE = "2026-06-07 02:07:16 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1780812436270L;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "af3b5ffa3854f277b781980c06ad3542cdc8d797";
+  public static final String GIT_DATE = "2026-06-07 03:05:36 GMT-04:00";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-06-07 03:35:35 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1780817735411L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
