d4c5f4fb4459d7b8e925bd49a9103bdebb523690
Jason <jy201016@gmail.com>
2026-06-14 11:44:06 +0800

    Added PID controller (with help from Claude). Probably needs more work

---
+++ src/main/java/org/curtinfrc/frc2026/BuildConstants.java
@@ -0,0 +1,17 @@
+package org.curtinfrc.frc2026;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "09ec2ba500b4dd78ba98e88a4335deab0ccce5a7";
+  public static final String GIT_DATE = "2026-06-13 23:23:14 GMT-04:00";
+  public static final String GIT_BRANCH = "jason/intake";
+  public static final String BUILD_DATE = "2026-06-13 23:40:59 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1781408459152L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
