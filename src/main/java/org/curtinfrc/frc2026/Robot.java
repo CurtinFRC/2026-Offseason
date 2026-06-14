5814a0b53a9c78e6c17ffac454ba2b496b66e0e4
Jason <jy201016@gmail.com>
2026-06-14 11:48:05 +0800

    Added intake to Comp part of Robot.java

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -18,6 +18,9 @@ import org.curtinfrc.frc2026.subsystems.drive.ModuleIO;
 import org.curtinfrc.frc2026.subsystems.drive.ModuleIOSim;
 import org.curtinfrc.frc2026.subsystems.drive.ModuleIOTalonFX;
 import org.curtinfrc.frc2026.subsystems.drive.TunerConstants;
+import org.curtinfrc.frc2026.subsystems.intake.Intake;
+import org.curtinfrc.frc2026.subsystems.intake.IntakeIO;
+import org.curtinfrc.frc2026.subsystems.intake.IntakeIOComp;
 import org.curtinfrc.frc2026.util.GameState;
 import org.curtinfrc.frc2026.util.PhoenixUtil;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -35,6 +38,7 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
  */
 public class Robot extends LoggedRobot {
   private Drive drive;
+  private Intake intake;
 
   private final XboxController controller = new XboxController(0);
   private final Alert controllerDisconnected =
@@ -90,6 +94,7 @@ public class Robot extends LoggedRobot {
                   new ModuleIOTalonFX(TunerConstants.FrontRight),
                   new ModuleIOTalonFX(TunerConstants.BackLeft),
                   new ModuleIOTalonFX(TunerConstants.BackRight));
+          intake = new Intake(new IntakeIOComp());
         }
         case SIM -> {
           drive =
