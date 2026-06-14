0772db419d60bcb0bc6b10cee3c1d775f2403c69
Eason-F <eason.ycfeng@outlook.com>
2026-06-14 11:05:23 +0800

    chore: fix formatting

--- src/main/java/org/curtinfrc/frc2026/subsystems/hopperindexer/IndexerRoller.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/hopperindexer/IndexerRoller.java
@@ -1,9 +1,8 @@
 package org.curtinfrc.frc2026.subsystems.hopperindexer;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class IndexerRoller extends SubsystemBase {
   private final IndexerRollerIO indexerRollerIO;
