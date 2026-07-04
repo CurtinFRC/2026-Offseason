903034fb4fd8a63761d8e2f3c6adfe3136010933
Eason-F <eason.ycfeng@outlook.com>
2026-07-05 00:05:47 +0800

    Update src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOSim.java
    Co-authored-by: Jade <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOSim.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ShooterIOSim extends ShooterIOComp {
-  private static final double DT = 0.02;
+  private static final double DT = 0.001
   private static final double SHOOTER_JKG = 0.0035858772; // temp
 
   private final TalonFXSimState motorSim;
