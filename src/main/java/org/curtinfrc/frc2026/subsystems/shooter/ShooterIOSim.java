ae2bcd1c50afa60e6697ecfc9d20b5646247bf7b
Eason-F <eason.ycfeng@outlook.com>
2026-07-05 07:28:32 +0800

    chore: fix formatting

--- src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOSim.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ShooterIOSim extends ShooterIOComp {
-  private static final double DT = 0.001
-  private static final double SHOOTER_JKG = 0.0035858772; // temp
+  private static final double DT = 0.001;
+  private static final double SHOOTER_JKG = 0.0035858772;
 
   private final TalonFXSimState motorSim;
   private final DCMotor motorType = DCMotor.getKrakenX60Foc(4);
