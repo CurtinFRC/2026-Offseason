4b1313fbaa0ec6041bbc64062c6d0f60200cc711
Eason-F <eason.ycfeng@outlook.com>
2026-07-16 13:36:32 +0800

    fix: gains from recalc

--- src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOComp.java
+++ src/main/java/org/curtinfrc/frc2026/subsystems/shooter/ShooterIOComp.java
@@ -35,8 +35,8 @@ public class ShooterIOComp implements ShooterIO {
   private static final double KI = 0.0;
   private static final double KD = 0.0;
   private static final double KS = 0.0;
-  private static final double KV = 0.154;
-  private static final double KA = 0.0124;
+  private static final double KV = 0.1232;
+  private static final double KA = 0.00992;
 
   protected final TalonFX leaderMotor = new TalonFX(ID1);
   protected final TalonFX followerMotor1 = new TalonFX(ID2);
