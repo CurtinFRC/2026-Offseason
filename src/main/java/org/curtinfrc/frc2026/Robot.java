17317925270a550fd118caf00c8575885c2cf285
Ghosted_Lol <9dingoes@gmail.com>
2026-07-19 12:42:06 +0800

    Auto routine unfinished

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -204,7 +204,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    autoChooser.periodic();
+  }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
