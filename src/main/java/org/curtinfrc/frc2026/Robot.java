bce5f6121e1bf138c713c49485850ef317823dba
Jason <jy201016@gmail.com>
2026-07-26 15:57:36 +0800

    Fixed TrenchAlign feature

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -185,7 +185,7 @@ public class Robot extends LoggedRobot {
         drive.joystickDrive(
             () -> -controller.getLeftY(),
             () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
+            () -> controller.getRightX()));
 
     controller
         .rightBumper()
@@ -194,9 +194,9 @@ public class Robot extends LoggedRobot {
     // intakeArm.setDefaultCommand(intakeArm.intake());
     controller.leftBumper().onTrue(intakeArm.intake());
     controller.rightTrigger().whileTrue(drive.alignToHub());
-    // controller
-    //     .leftTrigger()
-    //     .whileTrue(drive.TrenchAlign(() -> -controller.getLeftY(), () -> -controller.getLeftX()));
+    controller
+        .leftTrigger()
+        .whileTrue(drive.TrenchAlign(() -> -controller.getLeftY(), () -> -controller.getLeftX()));
     shooter
         .readyToIndex
         .onTrue(hopperIndexer.setAllRollerVoltage(6))
