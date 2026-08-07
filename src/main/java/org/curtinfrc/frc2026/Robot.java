0a3dc1f7148a97193d5fa021b67b87dd177d61bb
Hazzer890 <harrycassidy@protonmail.com>
2026-08-07 19:33:14 +0800

    Reapply shoot-button fix lost in merge 482419d: single composition, aligning flag wiring

--- src/main/java/org/curtinfrc/frc2026/Robot.java
+++ src/main/java/org/curtinfrc/frc2026/Robot.java
@@ -191,29 +191,24 @@ public class Robot extends LoggedRobot {
     shooter.setDefaultCommand(shooter.setVoltage(0));
     intakeArm.setDefaultCommand(intakeArm.intake());
 
-    controller
-        .rightBumper()
-        .whileTrue(Commands.parallel(shooter.setAngularVelocity(30), drive.alignToHub()))
-        .onFalse(intakeArm.intake());
-
+    // KEEP AS ONE BINDING. Two whileTrue bindings on the same button both requiring the shooter
+    // cancel each other, which kills alignToHub the cycle it starts (the "doesn't aim" bug).
+    // Likewise, a separate readyToShoot->feed trigger conflicts with the rollers used here.
     controller
         .rightBumper()
         .whileTrue(
             Commands.parallel(
+                drive.alignToHub(),
                 shooter.setAngularVelocity(30),
                 Commands.waitUntil(shooter.readyToShoot)
-                    .andThen(hopperIndexer.setAllRollerVoltage(6))));
+                    .andThen(hopperIndexer.setAllRollerVoltage(6))))
+        .onFalse(intakeArm.intake());
 
     controller.rightTrigger().whileTrue(intakeArm.outake());
     controller.leftBumper().whileTrue(intakeArm.push());
     controller
         .leftTrigger()
         .whileTrue(drive.TrenchAlign(() -> -controller.getLeftY(), () -> -controller.getLeftX()));
-    shooter
-        .readyToShoot
-        .and(drive.readyToShoot)
-        .onTrue(hopperIndexer.setAllRollerVoltage(6))
-        .onFalse(hopperIndexer.stopAll());
 
     autoFactory =
         new AutoFactory(drive::getPose, drive::setPose, drive::followTrajectory, true, drive);
